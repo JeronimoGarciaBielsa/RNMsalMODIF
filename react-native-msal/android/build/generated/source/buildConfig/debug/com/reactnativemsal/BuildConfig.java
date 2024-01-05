7441d1a8afbe51cea8e8d5d893c628cba07d6250
Jeronimo Garcia Bielsa <jerogarcia57@hotmail.com>
2024-01-05 09:21:08 +0100

    Modify Librery

---
+++ react-native-msal/android/build/generated/source/buildConfig/debug/com/reactnativemsal/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.reactnativemsal;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.reactnativemsal";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ react-native-msal/android/build/generated/source/buildConfig/release/com/reactnativemsal/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.reactnativemsal;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.reactnativemsal";
+  public static final String BUILD_TYPE = "release";
+}
